e2eacb1bc1359af0e986a34f6136e3a277eb2363
raja09016 <rajarajak2121raja@gmail.com>
2025-03-18 20:13:18 +0530

    add login and signup page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react'
-import { Routes ,Route} from 'react-router-dom'
-import Home from './pages/Home';
-import "./App.css"
-import Navbar from './components/Common/Navbar';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
+import Navbar from "./components/Common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 function App() {
   return (
-     <div  className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
-      <Navbar/>
-       <Routes>
-         <Route path="/" element={<Home/>}/>
-       </Routes>
-     </div>
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Open Route - for Only Non Logged in User */}
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
