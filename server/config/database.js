f189c8296117ed197f277a3b97882d2db678eb18
raja09016 <rajarajak2121raja@gmail.com>
2025-04-07 21:44:13 +0530

    database connection

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,10 @@ require("dotenv").config ;
 
 exports.connect = () =>{
     console.log("aaya");
-    mongoose.connect(process.env.MONGODB_URL) 
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlparser: true,
+        useUnifiedTopology: true,
+    }) 
     .then( ()=> { console.log("DB Conncted Successfully")})
     .catch( (error)=> {
          console.log("DB connection failed");
